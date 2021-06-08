6779b3a4529c8989f3ede82cdd92ec16fe34f5ce
Mattia Zoccarato <mattia.zoccarato@gmail.com>
2021-06-08 23:22:27 +0200

    update events xml

--- Block/Adminhtml/System/Config/ModuleVersion.php
+++ Block/Adminhtml/System/Config/ModuleVersion.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @category Chiron
+ * @package Chiron_Sirio
+ * @version 0.1.0
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 namespace Chiron\Sirio\Block\Adminhtml\System\Config;
 
--- registration.php
+++ registration.php
@@ -1,8 +1,11 @@
 <?php
 /**
- * @copyright: Copyright © 2017 AOE GmbH. All rights reserved.
- * @see LICENSE.txt
+ * @category Chiron
+ * @package Chiron_Sirio
+ * @version 0.1.0
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
